efd7c6bc67edc0c60f67149c06a7d952aecdda8b
kkkofi <kofiababio07@gmail.com>
2025-08-08 17:55:48 +0000

    First task done

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Animal genericAnimal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        genericAnimal.MakeSound(); 
+        dog.MakeSound();           
+        cat.MakeSound();           
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
